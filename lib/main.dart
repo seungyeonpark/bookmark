d6841d85bfdef3b95927f0d0b87124e4a84944bc
sypark <siaayeon@gmail.com>
2024-06-01 23:39:23 +0900

    feat: add webview

--- lib/main.dart
+++ lib/main.dart
@@ -174,6 +174,15 @@ class _BookmarkPageState extends State<BookmarkPage> {
     );
   }
 
+  void _openWebView(BuildContext context, String url) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => WebViewPage(url: url),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -213,6 +222,7 @@ class _BookmarkPageState extends State<BookmarkPage> {
                     ),
                   ),
                 ),
+                onTap: () => _openWebView(context, bookmarks[index]['link']!),
                 onLongPress: () {
                   showDialog(
                     context: context,
@@ -260,9 +270,157 @@ class _BookmarkPageState extends State<BookmarkPage> {
   }
 }
 
+class WebViewPage extends StatefulWidget {
+  final String url;
+
+  WebViewPage({required this.url});
+
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  final GlobalKey webViewKey = GlobalKey();
+  late final InAppWebViewController webViewController;
+  late final PullToRefreshController pullToRefreshController;
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pullToRefreshController = (kIsWeb
+        ? null
+        : PullToRefreshController(
+      options: PullToRefreshOptions(color: Colors.blue,),
+      onRefresh: () async {
+        if (defaultTargetPlatform == TargetPlatform.android) {
+          webViewController.reload();
+        } else if (defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.macOS) {
+          webViewController.loadUrl(urlRequest: URLRequest(url: await webViewController.getUrl()));}
+      },
+    ))!;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: SafeArea(
+            child: PopScope(
+                onPopInvoked: (shouldPop) async {
+                  if (await webViewController.canGoBack()) {
+                    webViewController.goBack();
+                  } else {
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Column(children: <Widget>[
+                  progress < 1.0
+                      ? LinearProgressIndicator(value: progress, color: Colors.blue)
+                      : Container(),
+                  Expanded(
+                      child: Stack(children: [
+                        InAppWebView(
+                          key: webViewKey,
+                          initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
+                          initialOptions: InAppWebViewGroupOptions(
+                            crossPlatform: InAppWebViewOptions(
+                                javaScriptCanOpenWindowsAutomatically: true,
+                                javaScriptEnabled: true,
+                                useOnDownloadStart: true,
+                                useOnLoadResource: true,
+                                useShouldOverrideUrlLoading: true,
+                                mediaPlaybackRequiresUserGesture: true,
+                                allowFileAccessFromFileURLs: true,
+                                allowUniversalAccessFromFileURLs: true,
+                                verticalScrollBarEnabled: true,
+                                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+                            ),
+                            android: AndroidInAppWebViewOptions(
+                                useHybridComposition: true,
+                                allowContentAccess: true,
+                                builtInZoomControls: true,
+                                thirdPartyCookiesEnabled: true,
+                                allowFileAccess: true,
+                                supportMultipleWindows: true
+                            ),
+                            ios: IOSInAppWebViewOptions(
+                              allowsInlineMediaPlayback: true,
+                              allowsBackForwardNavigationGestures: true,
+                            ),
+                          ),
+                          pullToRefreshController: pullToRefreshController,
+                          onLoadStart: (InAppWebViewController controller, uri) {
+                            setState(() {
+                              // Do nothing for now
+                            });
+                          },
+                          onLoadStop: (InAppWebViewController controller, uri) {
+                            setState(() {
+                              // Do nothing for now
+                            });
+                          },
+                          onProgressChanged: (controller, progress) {
+                            if (progress == 100) {
+                              pullToRefreshController.endRefreshing();
+                            }
+                            setState(() {
+                              this.progress = progress / 100;
+                            });
+                          },
+                          androidOnPermissionRequest: (controller, origin, resources) async {
+                            return PermissionRequestResponse(
+                                resources: resources,
+                                action: PermissionRequestResponseAction.GRANT);
+                          },
+                          onWebViewCreated: (InAppWebViewController controller) {
+                            webViewController = controller;
+                          },
+                          onCreateWindow: (controller, createWindowRequest) async{
+                            showDialog(
+                              context: context, builder: (context) {
+                              return AlertDialog(
+                                content: SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 400,
+                                  child: InAppWebView(
+                                    // Setting the windowId property is important here!
+                                    windowId: createWindowRequest.windowId,
+                                    initialOptions: InAppWebViewGroupOptions(
+                                      android: AndroidInAppWebViewOptions(
+                                        builtInZoomControls: true,
+                                        thirdPartyCookiesEnabled: true,
+                                      ),
+                                      crossPlatform: InAppWebViewOptions(
+                                          cacheEnabled: true,
+                                          javaScriptEnabled: true,
+                                          userAgent: "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"
+                                      ),
+                                      ios: IOSInAppWebViewOptions(
+                                        allowsInlineMediaPlayback: true,
+                                        allowsBackForwardNavigationGestures: true,
+                                      ),
+                                    ),
+                                    onCloseWindow: (controller) async{
+                                      if (Navigator.canPop(context)) {
+                                        Navigator.pop(context);
+                                      }
+                                    },
+                                  ),
+                                ),);
+                            },
+                            );
+                            return true;
+                          },
+                        )
+                      ]))
+                ])
+            )
+        )
+    );
   }
 }
