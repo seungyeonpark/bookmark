b61cff04432d3221d35354aca8678246345c49fd
sypark <siaayeon@gmail.com>
2024-06-09 02:02:14 +0900

    refactor: simplify webview methods and settings

--- lib/web.dart
+++ lib/web.dart
@@ -22,7 +22,7 @@ class _WebViewPageState extends State<WebViewPage> {
     super.initState();
 
     pullToRefreshController = PullToRefreshController(
-      options: PullToRefreshOptions(
+      settings: PullToRefreshSettings(
         color: Colors.blue,
       ),
       onRefresh: () async {
@@ -38,132 +38,22 @@ class _WebViewPageState extends State<WebViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: SafeArea(
-        child: WillPopScope(
-          onWillPop: () async {
-            if (await webViewController.canGoBack()) {
-              webViewController.goBack();
-              return false;
-            }
-            return true;
-          },
-          child: Column(
-            children: <Widget>[
-              progress < 1.0
-                  ? LinearProgressIndicator(
-                value: progress,
-                color: Colors.blue,
-              )
-                  : Container(),
-              Expanded(
-                child: Stack(
-                  children: [
-                    InAppWebView(
-                      key: webViewKey,
-                      initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
-                      initialOptions: InAppWebViewGroupOptions(
-                        crossPlatform: InAppWebViewOptions(
-                          javaScriptCanOpenWindowsAutomatically: true,
-                          javaScriptEnabled: true,
-                          useOnDownloadStart: true,
-                          useOnLoadResource: true,
-                          useShouldOverrideUrlLoading: true,
-                          mediaPlaybackRequiresUserGesture: true,
-                          allowFileAccessFromFileURLs: true,
-                          allowUniversalAccessFromFileURLs: true,
-                          verticalScrollBarEnabled: true,
-                          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
-                        ),
-                        android: AndroidInAppWebViewOptions(
-                          useHybridComposition: true,
-                          allowContentAccess: true,
-                          builtInZoomControls: true,
-                          thirdPartyCookiesEnabled: true,
-                          allowFileAccess: true,
-                          supportMultipleWindows: true,
-                        ),
-                        ios: IOSInAppWebViewOptions(
-                          allowsInlineMediaPlayback: true,
-                          allowsBackForwardNavigationGestures: true,
-                        ),
-                      ),
-                      pullToRefreshController: pullToRefreshController,
-                      onLoadStart: (InAppWebViewController controller, uri) {
-                        setState(() {
-                          // Do nothing for now
-                        });
-                      },
-                      onLoadStop: (InAppWebViewController controller, uri) {
-                        setState(() {
-                          // Do nothing for now
-                        });
-                      },
-                      onProgressChanged: (controller, progress) {
-                        if (progress == 100) {
-                          pullToRefreshController.endRefreshing();
-                        }
-                        setState(() {
-                          this.progress = progress / 100;
-                        });
-                      },
-                      androidOnPermissionRequest: (controller, origin, resources) async {
-                        return PermissionRequestResponse(
-                          resources: resources,
-                          action: PermissionRequestResponseAction.GRANT,
-                        );
-                      },
-                      onWebViewCreated: (InAppWebViewController controller) {
-                        webViewController = controller;
-                      },
-                      onCreateWindow: (controller, createWindowRequest) async {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              content: SizedBox(
-                                width: MediaQuery.of(context).size.width,
-                                height: 400,
-                                child: InAppWebView(
-                                  // Setting the windowId property is important here!
-                                  windowId: createWindowRequest.windowId,
-                                  initialOptions: InAppWebViewGroupOptions(
-                                    android: AndroidInAppWebViewOptions(
-                                      builtInZoomControls: true,
-                                      thirdPartyCookiesEnabled: true,
-                                    ),
-                                    crossPlatform: InAppWebViewOptions(
-                                      cacheEnabled: true,
-                                      javaScriptEnabled: true,
-                                      userAgent: "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
-                                    ),
-                                    ios: IOSInAppWebViewOptions(
-                                      allowsInlineMediaPlayback: true,
-                                      allowsBackForwardNavigationGestures: true,
-                                    ),
-                                  ),
-                                  onCloseWindow: (controller) async {
-                                    if (Navigator.canPop(context)) {
-                                      Navigator.pop(context);
-                                    }
-                                  },
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                        return true;
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: WebUri(widget.url)),
+        onWebViewCreated: (controller) {
+          webViewController = controller;
+        },
+        onReceivedServerTrustAuthRequest: (controller, challenge) async {
+          return ServerTrustAuthResponse(action: ServerTrustAuthResponseAction.PROCEED);
+        },
+        shouldOverrideUrlLoading: (controller, navigationAction) async {
+          var uri = navigationAction.request.url!;
+          if (!["http", "https", "file", "chrome", "data", "javascript", "about"].contains(uri.scheme)) {
+            return NavigationActionPolicy.CANCEL;
+          }
+          return NavigationActionPolicy.ALLOW;
+        },
       ),
     );
   }
